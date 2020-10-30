4b5d66757e6d4b8831eeaad6ed5c42740303ceb6
Piradon-lab <71110711+Piradon-lab@users.noreply.github.com>
2020-10-30 19:51:16 +0700

    Update weeb.php

--- weeb.php
+++ weeb.php
@@ -11,7 +11,7 @@
         <a href="https://www.netflix.com/search?q=my%20hero&jbv=80135674">This is my favorite link</a>
 
         <h2>My Joke</h2>
-        <img src="meme.jpg">
+        <img src="https://scontent.fbkk5-5.fna.fbcdn.net/v/t1.15752-0/p280x280/118691083_322225429127654_4215542645130633860_n.jpg?_nc_cat=100&ccb=2&_nc_sid=ae9488&_nc_eui2=AeHoEVG6TzDYeVf9JP2EK_sbWtW8E_xHr01a1bwT_EevTc-fmI7r8UrTfHLUep7y03bPTgfXC9OCiLL4So33FPBp&_nc_ohc=EBePc4hVdBkAX-SHBT4&_nc_ht=scontent.fbkk5-5.fna&tp=6&oh=9baf25cf386d78b67993a2ab61bf0800&oe=5FC12B01">
 
     </body>
 </html>

098e234099c96d8f6c26e1fcc2454cd8d1a9a042
Piradon-lab <71110711+Piradon-lab@users.noreply.github.com>
2020-10-30 19:43:12 +0700

    Update weeb.php

--- weeb.php
+++ weeb.php
@@ -6,12 +6,12 @@
     <body>
         <h1>Name: Piradon Phuengphong</h1>
         <p>Nickname: Don</p>
-        <img src = "https://i.ytimg.com/vi/gSo9E5FbbOg/maxresdefault.jpg">
+        <img src="https://i.ytimg.com/vi/gSo9E5FbbOg/maxresdefault.jpg">
         <br>
         <a href="https://www.netflix.com/search?q=my%20hero&jbv=80135674">This is my favorite link</a>
 
         <h2>My Joke</h2>
-        <img src = "meme.jpg">
+        <img src="meme.jpg">
 
     </body>
 </html>
